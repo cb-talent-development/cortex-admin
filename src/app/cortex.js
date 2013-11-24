7c6dc12e53ba1d01801527ce8f5564aa5293fdbb
Bennett Goble <Bennett.Goble@careerbuilder.com>
2013-11-24 15:48:15 -0500

    Adjusted default Accept header

--- src/app/cortex.js
+++ src/app/cortex.js
@@ -7,11 +7,17 @@ var cortexModule = angular.module('cortex', [
     'cortex.states.users.login'
 ]);
 
-cortexModule.config(function ($urlRouterProvider) {
+cortexModule.config(function ($urlRouterProvider, $httpProvider) {
 
     $urlRouterProvider.when('/admin/assets', '/admin/assets/');
 
     $urlRouterProvider.otherwise('/login');
+
+    // override the defalt Accept header value of 'application/json, text/plain, */*'
+    // as "*/*" invalidates all specificity
+    // https://github.com/rails/rails/issues/9940
+    // http://blog.bigbinary.com/2010/11/23/mime-type-resolution-in-rails.html
+    $httpProvider.defaults.headers.common['Accept'] = 'application/json, text/plain';
 });
 
 cortexModule.controller('CortexAdminCtrl', function ($scope, $state) {
