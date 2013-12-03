cbaeba21ab08b3fa2a1db45b2eed6292a28ee598
Alex Tharp <alex.tharp@careerbuilder.com>
2013-12-03 17:31:57 -0500

    Implement basic http interceptor for failed requests and responses that contain rejections

--- src/app/cortex.js
+++ src/app/cortex.js
@@ -7,17 +7,33 @@ var cortexModule = angular.module('cortex', [
     'cortex.states.users.login'
 ]);
 
+cortexModule.factory('httpInterceptorService', function($q) {
+    return {
+        'requestError': function(rejection) {
+            // Perhaps retry here
+            alert('HTTP request encountered an error. Not retrying. In the future: implement retries, logging stack trace, and preventing duplicate requests.');
+            return $q.reject(rejection);
+        },
+        'responseError': function(rejection) {
+            alert('It would broadcast an error here on the rootScope');
+            return $q.reject(rejection);
+        }
+    };
+});
+
 cortexModule.config(function ($urlRouterProvider, $httpProvider) {
 
     $urlRouterProvider.when('/admin/assets', '/admin/assets/');
 
     $urlRouterProvider.otherwise('/login');
 
-    // override the defalt Accept header value of 'application/json, text/plain, */*'
+    // override the default Accept header value of 'application/json, text/plain, */*'
     // as "*/*" invalidates all specificity
     // https://github.com/rails/rails/issues/9940
     // http://blog.bigbinary.com/2010/11/23/mime-type-resolution-in-rails.html
     $httpProvider.defaults.headers.common['Accept'] = 'application/json, text/plain';
+
+    $httpProvider.interceptors.push('httpInterceptorService');
 });
 
 cortexModule.controller('CortexAdminCtrl', function ($scope, $state) {
