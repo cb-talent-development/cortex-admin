0d93f1b4e38ac79393cea648a24b854bdb82bacf
Alex Tharp <alex.tharp@careerbuilder.com>
2013-12-04 14:29:33 -0500

    Correct capitalization of Config constant when mocking it up for Organizations resource test

--- src/app/resources/organizations.spec.js
+++ src/app/resources/organizations.spec.js
@@ -83,7 +83,7 @@ describe('Organizations Resource Module', function(){
         beforeEach(function(){
             angular.mock.module('cortex.resources.authorized');
             angular.mock.module(function($provide){
-                $provide.constant('config', {api: {baseUrl: ''}});
+                $provide.constant('Config', {api: {baseUrl: ''}});
             });
 
             inject(function($injector){
