737d9349b5cd287c8a39e8de682d663ad57909f8
Bennett Goble <Bennett.Goble@careerbuilder.com>
2013-12-05 11:48:46 -0500

    Added contact_email to tenant creation, removed console.log from tenantSettings directive

--- src/app/directives/tenantSettings.js
+++ src/app/directives/tenantSettings.js
@@ -3,9 +3,6 @@ var module = angular.module('cortex.directives.tenantSettings', []);
 module.directive('tenantSettings', function() {
     return {
         restrict: 'EA',
-        templateUrl: 'directives/tenantSettings.tpl.html',
-        link: function(){
-            console.log('tenantSettings');
-        }
+        templateUrl: 'directives/tenantSettings.tpl.html'
     };
 });
