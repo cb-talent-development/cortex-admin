91842017ed36f0fed4afc97a63fb4edc9e5be8b7
Alex Tharp <alex.tharp@careerbuilder.com>
2013-12-23 14:13:55 -0500

    Basic Edit Asset functionality. Flash HTTP errors instead of alert box.

--- src/app/states/admin/assets/edit/edit.js
+++ src/app/states/admin/assets/edit/edit.js
@@ -1,5 +1,6 @@
 var module = angular.module('cortex.states.admin.assets.edit', [
     'ui.router.state',
+    'angular-flash.service',
     'common.filters',
     'cortex.resources.assets'
 ]);
@@ -12,6 +13,21 @@ module.config(function($stateProvider){
     });
 });
 
-module.controller('AssetsEditCtrl', function($scope, $stateParams, Assets) {
-    $scope.asset = Assets.get({id: $stateParams.assetId});
+module.controller('AssetsEditCtrl', function($scope, $stateParams, $filter, flash, Assets) {
+    $scope.data = {};
+
+    $scope.asset = Assets.get({id: $stateParams.assetId}, function(asset) {
+        $scope.data.tags = $filter('tagList')(asset.tags);
+    });
+
+    $scope.save = function() {
+        $scope.asset.tag_list = $scope.data.tags;
+        delete $scope.asset.tags;
+
+        $scope.asset.$save(function(asset) {
+            var message = 'Saved asset "' +  asset.name + '"';
+
+            flash.success = asset.tag_list;
+        });
+    };
 });
