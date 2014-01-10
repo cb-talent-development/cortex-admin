c36aedd193c14fc84456cca99616c99d2c214703
Alex Tharp <alex.tharp@careerbuilder.com>
2014-01-10 12:19:55 -0500

    Update environment configs to reflect new C66 envs

--- src/app/config.js
+++ src/app/config.js
@@ -1,22 +1,35 @@
 var configModule = angular.module('cortex.config', []);
 
-var devConfig = {
-    api: {
-        baseUrl: 'http://localhost:3000',
-        environmentName: 'Development'
-    }
-};
-
-var prodConfig = {
-    api: {
-        baseUrl: 'http://whale.cb-cortex.c66.me',
-        environmentName: 'Production'
+var environmentConfigs = {
+    localConfig: {
+        api: {
+            baseUrl: 'http://localhost:3000',
+            environmentName: 'Local'
+        }
+    },
+    devConfig: {
+        api: {
+            baseUrl: 'http://dev.api.cbcortex.com',
+            environmentName: 'Development'
+        }
+    },
+    stgConfig: {
+        api: {
+            baseUrl: 'http://staging.api.cbcortex.com',
+            environmentName: 'Staging'
+        }
+    },
+    prdConfig: {
+        api: {
+            baseUrl: 'http://api.cbcortex.com',
+            environmentName: 'Production'
+        }
     }
 };
 
 // In the future this will be replaced with a grunt step to provide
 // environment specific configuration.
-configModule.constant('config', devConfig);
+configModule.constant('config', environmentConfigs.localConfig);
 
 configModule.constant('events', {
     STATE_CHANGE_SUCCESS: '$stateChangeSuccess',
