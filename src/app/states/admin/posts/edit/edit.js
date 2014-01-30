a4125c8bb8795e44be6581994c1b72188de108d2
Carlos Gonzalez <carlos.gonzalez@careerbuilder.com>
2014-01-30 18:23:36 -0500

    added ui.bootstrap.buttons dependency to admin/posts/edit/edit.js

--- src/app/states/admin/posts/edit/edit.js
+++ src/app/states/admin/posts/edit/edit.js
@@ -1,6 +1,7 @@
 var module = angular.module('cortex.states.admin.posts.edit', [
     'ngCookies',
     'ui.router.state',
+    'ui.bootstrap.buttons',
     'ui.bootstrap.datepicker',
     'cortex.config',
     'cortex.services.auth',
