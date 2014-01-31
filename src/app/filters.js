5d31597f7ad18b76337e25cff33965d6048f2a4b
Carlos Gonzalez <carlos.gonzalez@careerbuilder.com>
2014-01-31 17:22:07 -0500

    Changed "term" for "string" to fix humanize filter

--- src/app/filters.js
+++ src/app/filters.js
@@ -2,7 +2,7 @@ var module = angular.module('cortex.filters', []);
 
 module.filter('humanize', function() {
     return function(string) {
-        string = term.split('_').join(' ').toLowerCase();
+        string = string.split('_').join(' ').toLowerCase();
         return string.charAt(0).toUpperCase() + string.slice(1);   
     };
 });
