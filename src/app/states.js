5f6bd2111e2092b381edc5485fb9736d3748148c
Alex Tharp <alex.tharp@careerbuilder.com>
2014-02-10 18:07:04 -0500

    Improve tenants.edit and .new states. organizations.edit and .new still need work - we'll need a larger refactor of that nested structure later - the refactor was too large for me to commit to right now.

--- src/app/states.js
+++ src/app/states.js
@@ -112,14 +112,20 @@ module.config(function ($stateProvider) {
             controller: 'OrganizationsCtrl'
         })
 
-        .state('admin.organizations.edit', {
+        .state('admin.organizations.tenants', {
             url: '',
             template: '<ui-view/>',
             abstract: true
         })
 
-        .state('admin.organizations.edit.tenants', {
+        .state('admin.organizations.tenants.edit', {
             url: '/tenants/:tenantId/edit',
+            templateUrl: 'views/admin/tenants/edit.tpl.html',
+            controller: 'EditTenantsCtrl'
+        })
+
+        .state('admin.organizations.tenants.new', {
+            url: '/tenants/new',
             templateUrl: 'views/admin/tenants/edit.tpl.html',
             controller: 'EditTenantsCtrl'
         })
